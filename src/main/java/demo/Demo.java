cc4f4a437beb527ab2376b717ac4c9c630d7c892
bj <bj@126.com>
2019-04-21 11:19:42 +0800

    first commit local repository

---
+++ src/main/java/demo/Demo.java
@@ -0,0 +1,16 @@
+package demo;
+
+/**
+ * className:Demo
+ * package:${PACKAGE_MAME}
+ * Description:
+ *
+ * @data:2019-04-21 11:04
+ * @author:tengjingyao
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("哈哈");
+    }
+
+}
