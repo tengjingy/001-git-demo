4af7cd2fb568d686804208e3033b686a44671555
bj <bj@126.com>
2019-04-21 11:40:45 +0800

    first commit local repository

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -11,6 +11,8 @@ package demo;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("哈哈");
+        System.out.println("哈哈");
+        System.out.println("哈哈");
     }
 
 }
